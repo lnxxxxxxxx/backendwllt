0d497e0d27c5b4298d4bbc8ae72433411f040fb1
Leandro <leandro.luna25@hotmail.com>
2023-11-17 23:11:32 -0300

    as

--- src/main/java/com/challengedbackend/challengedbackend/Config/CorsConfig.java
+++ src/main/java/com/challengedbackend/challengedbackend/Config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("*", "backendwllt-production.up.railway.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.addAllowedHeader("*");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
